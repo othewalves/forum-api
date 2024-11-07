00ad194c2330d927d6cd784227460e4b10f893dc
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-11-07 19:26:55 -0300

    feat: signup user

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,4 +8,4 @@ import { DatabaseModule } from './database/database.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,30 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
+
+import { PrismaService } from 'src/database/prisma.service';
+
+
 
 @Injectable()
 export class UserService {
 
     @Inject()
-    private readonly prisma: PrismaClient
+    private readonly prisma: PrismaService
 
     async createUser(data: Prisma.UserCreateInput) {
         return this.prisma.user.create({ data })
+    }
+
+    async updateUser(params: {
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput
+    }): Promise<User> {
+        const { where, data } = params;
+
+        return this.prisma.user.update({
+            data,
+            where
+        })
     }
 
 }
